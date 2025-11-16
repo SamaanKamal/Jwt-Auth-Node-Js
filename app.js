34374cab8abeff2551d2ecc08903e2d86c886a25
Samaan Khella (EXT-Nokia) <samaan.khella.ext@nokia.com>
2025-11-16 15:14:29 +0200

    initialized the project

---
+++ app.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const express = require('express');
+const cors = require('cors');
+const taskRoutes = require('./routes/taskRoutes');
